fb34a36d8a36efa5945b08f0bed85deea8573a7f
alzbetahladikova <beta.strelbova@gmail.com>
2023-04-11 19:18:18 +0200

    Přidány prvky

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -11,8 +11,15 @@ public class Aplikace extends JFrame {
 
     private JLabel husyLabel;
     private JLabel kraliciLabel;
+
+    private JLabel hlavyLabel;
+    private JLabel nohyLabel;
     private JTextField husyField;
     private JTextField kraliciField;
+    private JTextField hlavyField;
+    private JTextField nohyField;
+
+    private JButton vypocitejButton;
 
     public static void main(String[] args) {
         FlatLightLaf.setup();
@@ -52,10 +59,31 @@ public class Aplikace extends JFrame {
         add(kraliciField);
 
 
+        add(createButtonBar(), "span");
 
+        hlavyField= new JTextField();
+        hlavyLabel = new JLabel("Počet hlav");
+        hlavyLabel.setDisplayedMnemonic('h');
+        hlavyLabel.setLabelFor(hlavyField);
+        add(hlavyLabel);
+        add(hlavyField);
 
-
+        nohyField= new JTextField();
+        nohyLabel = new JLabel("Počet nohou");
+        nohyLabel.setDisplayedMnemonic('N');
+        nohyLabel.setLabelFor(nohyField);
+        add(nohyLabel);
+        add(nohyField);
         pack();
-    }
 
+
+    }
+    private JPanel createButtonBar() {
+        vypocitejButton = new JButton("Vypočítej");
+        vypocitejButton.setMnemonic('V');
+
+        JPanel buttonBar = new JPanel(new MigLayout(null, "[right, grow]"));
+        buttonBar.add(vypocitejButton);
+        return buttonBar;
+    }
 }
